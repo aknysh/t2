8f9de653f10ef2c0c7c4618e1946c2015a4e9a8e
aknysh <andriy.knysh@gmail.com>
2020-01-22 15:10:46 -0500

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "dynamodb_label" {
   source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
   enabled     = var.enabled
-  namespace   = var.namespace
+  namespace   =   var.namespace
   stage       = var.stage
   environment = var.environment
   name        = var.name
   delimiter   = var.delimiter
   attributes  = var.attributes
-  tags        = var.tags
+  tags        =   var.tags
 }
